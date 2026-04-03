e8bf985f7412ba681e7f486116b9d35830d8c5ea
cameornh <crhalaby652@gmail.com>
2026-04-02 21:10:36 -0400

    Next.js and Builder setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import BuilderDevTools from "@builder.io/dev-tools/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = BuilderDevTools()({
+  /* GitHub Pages requires a static export */
+  output: "export",
+  
+  /* GitHub Pages doesn't support Next.js's built-in image optimization server */
+  images: {
+    unoptimized: true,
+  },
+});
 
 export default nextConfig;
